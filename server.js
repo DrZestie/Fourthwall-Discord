001a8b5aedd59505159f100b64ef73ee81728a23
DrZestie <wnicholas963@gmail.com>
2026-05-21 22:17:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-const DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1506460070978785372/_fknexXv1ft2rLqx55Pr0Bp4ITNjSyQrurUcjeUw8rDW8at1oh_rDMfS59TMGXO1rkZ-";
+const DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1507205538147074130/h7kKhQS-AbGGMEtmcZF2y2TjMZEDZOk2cAn6HgpQGA-jCYL_7OCJ8q8VrB8Fx85Mikzu";
 
 const processedOrders = new Set();
 
